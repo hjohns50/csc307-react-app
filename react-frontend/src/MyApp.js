6b435985ea6028d45dce96ce93c2e49d50ada4b4
hjohns50 <henryjohnson@comcast.net>
2023-09-30 15:34:03 -0700

    Added Form

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -3,13 +3,8 @@ import React, {useState} from 'react';
 import Table from './Table';
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-            name: 'Charlie',
-            job: 'Janitor',
-        },
-      // the rest of the data
-    ]); 
+    
+    const [characters, setCharacters] = useState([]);
     
     function removeOneCharacter (index) {
 	    const updated = characters.filter((character, i) => {
